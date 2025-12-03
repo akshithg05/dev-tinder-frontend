1780ec893b023b9d28c23d9b9cea44b85c6f234d
Akshith G <akshithg01@gmail.com>
2025-12-03 07:26:33 +0530

    Changing backend url

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,2 @@
-export const BASE_URL = "http://localhost:3000";
-// export const BASE_URL = "/api";
-//export const BASE_URL = "https://dev-tinder-backend-r1ek.onrender.com";
+// export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://dev-tinder-backend-r1ek.onrender.com";
